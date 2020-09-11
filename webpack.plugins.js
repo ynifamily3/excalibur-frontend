2ad7d2273c71e06ab2a31396823a1cee90a1c658
Jongkeun Choi <jongkeun.ch@gmail.com>
2020-09-11 15:30:44 +0900

    docs: 빌드 과정 중 냥캣 로딩 추가
     

--- webpack.plugins.js
+++ webpack.plugins.js
@@ -1,4 +1,9 @@
 const ForkTsCheckerWebpackPlugin = require("fork-ts-checker-webpack-plugin");
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const NyanProgressPlugin = require("nyan-progress-webpack-plugin");
 
-module.exports = [new ForkTsCheckerWebpackPlugin(), new CleanWebpackPlugin()];
+module.exports = [
+  new ForkTsCheckerWebpackPlugin(),
+  new CleanWebpackPlugin(),
+  new NyanProgressPlugin(),
+];
